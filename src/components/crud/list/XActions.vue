fa42aef31beafa7b7184e27a90225d06fca7e985
Rafael Arend <rafinhaarend123@hotmail.com>
2023-03-24 18:57:03 -0400

    Implementando ações da listagem em modo flutuante

--- src/components/crud/list/XActions.vue
+++ src/components/crud/list/XActions.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="row justify-between items-center q-gap-md">
+  <div class="row items-center q-gap-md">
 
     <q-input
       v-model="xList.filter"
       :placeholder="xList.filterPlaceholder"
-      class="col-md-5 col-xs-12"
+      class="col-md-grow col-xs-12"
       debounce="300"
       dense
       outlined
@@ -15,45 +15,85 @@
       </template>
     </q-input>
 
-    <div class="row q-gap-sm">
-      <XBtn
-        @click="xList.destroy"
-        :disable="!xList.selectedRows.length"
-        v-if="auth.hasPermission(`${xList.permissionsGroupName}_delete`)"
-        tooltip-label="Excluir"
-        icon="mdi-close"
-        color="red"
-      />
-      <XBtn
-        @click="xList.edit"
-        :disable="!(xList.selectedRows.length === 1)"
-        v-if="auth.hasPermission(`${xList.permissionsGroupName}_edit`)"
-        tooltip-label="Editar"
-        icon="mdi-pencil"
-      />
-      <XBtn
-        @click="xList.view"
-        :disable="!(xList.selectedRows.length === 1)"
-        v-if="auth.hasPermission(`${xList.permissionsGroupName}_view`)"
-        tooltip-label="Visualizar"
-        icon="mdi-eye"
-      />
-      <XBtn
-        :to="{ name: `${xList.routeGroupName}-create` }"
-        v-if="auth.hasPermission(`${xList.permissionsGroupName}_create`)"
-        tooltip-label="Adicionar"
-        icon="mdi-plus"
-      />
+    <q-space />
+
+    <div class="col-md-auto col-xs-12 row q-gap-sm">
       <XBtn
         @click="xList.toggleFullscreenMode()"
         :icon="xList.fullscreenModeIcon"
         :tooltip-label="xList.fullscreenModeTooltipLabel"
+        tooltip-position="top"
+        dense
       />
       <XBtn
         @click="xList.toggleGridMode()"
         :tooltip-label="xList.gridModeTooltipLabel"
+        tooltip-position="top"
         icon="mdi-grid"
+        dense
       />
+      <q-fab
+        :class="$q.screen.lt.md ? 'order-first' : ''"
+        color="primary"
+        icon="keyboard_arrow_down"
+        direction="down"
+        padding="sm"
+        square
+      >
+        <template #tooltip>
+          <XTooltip
+            label="Ações"
+            position="top"
+          />
+        </template>
+        <q-fab-action
+          :to="{ name: `${xList.routeGroupName}-create` }"
+          v-if="auth.hasPermission(`${xList.permissionsGroupName}_create`)"
+          icon="mdi-plus"
+          color="primary"
+        >
+          <XTooltip
+            label="Adicionar"
+            position="top"
+          />
+        </q-fab-action>
+        <q-fab-action
+          @click="xList.view"
+          :disable="!(xList.selectedRows.length === 1)"
+          v-if="auth.hasPermission(`${xList.permissionsGroupName}_view`)"
+          icon="mdi-eye"
+          color="primary"
+        >
+          <XTooltip
+            label="Visualizar"
+            position="top"
+          />
+        </q-fab-action>
+        <q-fab-action
+          @click="xList.edit"
+          :disable="!(xList.selectedRows.length === 1)"
+          v-if="auth.hasPermission(`${xList.permissionsGroupName}_edit`)"
+          icon="mdi-pencil"
+          color="primary"
+        >
+          <XTooltip
+            label="Editar"
+            position="top"
+          />
+        </q-fab-action>
+        <q-fab-action
+          @click="xList.destroy"
+          :disable="!xList.selectedRows.length"
+          v-if="auth.hasPermission(`${xList.permissionsGroupName}_delete`)"
+          icon="mdi-close"
+          color="red"
+        >
+          <XTooltip
+            label="Excluir"
+            position="top"
+          />
+        </q-fab-action>
+      </q-fab>
     </div>
 
   </div>
@@ -63,6 +103,7 @@
 import { useXListStore } from 'src/stores/xList'
 import { useAuthStore } from 'src/stores/auth'
 import XBtn from 'src/components/buttons/XBtn.vue'
+import XTooltip from 'src/components/others/XTooltip.vue'
 
 const xList = useXListStore()
 const auth = useAuthStore()
